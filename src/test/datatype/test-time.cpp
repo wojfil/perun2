b9d6a7bea54b4b340b6b17ebc80b26d3107715f1
wojfil <wojfil@protonmail.com>
2022-06-22 19:35:09 +0200

    Added more time tests

--- src/test/datatype/test-time.cpp
+++ src/test/datatype/test-time.cpp
@@ -25,7 +25,17 @@ void test_time()
     testCase_timeShift(1, _tim(3, 6, 2000), _per(12, Period::u_Days), _tim(15, 6, 2000));
     testCase_timeShift(2, _tim(29, 6, 2020), _per(5, Period::u_Days), _tim(4, 7, 2020));
     testCase_timeShift(3, _tim(28, 2, 2020), _per(3, Period::u_Days), _tim(2, 3, 2020));
-    // todo add more
+    testCase_timeShift(4, _tim(21, 6, 2022), _per(3, Period::u_Weeks), _tim(12, 7, 2022));
+    testCase_timeShift(5, _tim(21, 6, 2022), _per(10, Period::u_Years), _tim(21, 6, 2032));
+    testCase_timeShift(6, _tim(29, 2, 2020), _per(1, Period::u_Years), _tim(28, 2, 2021));
+    testCase_timeShift(7, _tim(28, 2, 2020), _per(60, Period::u_Days), _tim(28, 4, 2020));
+    testCase_timeShift(8, _tim(16, 9, 2021), _per(31, Period::u_Minutes), _tim(17, 9, 2021, 0, 31));
+    testCase_timeShift(9, _tim(16, 9, 2021), _per(-31, Period::u_Minutes), _tim(15, 9, 2021, 23, 29));
+    testCase_timeShift(10, _tim(21, 11, 2021), _per(4, Period::u_Months), _tim(21, 3, 2022));
+    testCase_timeShift(11, _tim(21, 11, 2021, 10, 15), _per(4, Period::u_Hours), _tim(21, 11, 2021, 14, 15));
+    testCase_timeShift(12, _tim(21, 11, 2021, 10, 15), _per(23, Period::u_Hours), _tim(22, 11, 2021, 9, 15));
+    testCase_timeShift(13, _tim(21, 11, 2021, 10, 15, 16), _per(2, Period::u_Seconds), _tim(21, 11, 2021, 10, 15, 18));
+    testCase_timeShift(14, _tim(21, 11, 2021, 10, 15, 0), _per(86340, Period::u_Seconds), _tim(22, 11, 2021, 10, 14, 0));
 }
 
 void testCase_timeShift(const _int& caseId, const _tim& value, const _per& shift, const _tim& expectedResult)

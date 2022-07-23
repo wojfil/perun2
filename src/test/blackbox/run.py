b0fb24b6324254b9b496e6df0e866a1f2c972010
wojfil <wojfil@protonmail.com>
2022-07-23 23:55:49 +0200

    Working location of black-box tests moved to a relative directory 'res'

--- src/test/blackbox/run.py
+++ src/test/blackbox/run.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def runTestCase(code, expectedResult):
-  cmd = "uro -c \"" + code + "\""
+  cmd = "uro -d res -c \"" + code + "\""
   output = subprocess.getoutput(cmd)
   if output != expectedResult:
     print("Test failed at running code: " + code)
@@ -18,6 +18,9 @@ runTestCase("print 'hello world'", "hello world")
 runTestCase("print 2 +2*2", "6")
 runTestCase("print 2, 5", lines("2", "5"))
 runTestCase("print 2, 2*25, 1", lines("2", "50", "1"))
+runTestCase("if true {43} else {21}", "43")
+runTestCase("if true and false {43} else {21}", "21")
+runTestCase(" 'a.txt' {  exists; size }  ", lines("1", "47"))
 	
 print ("BLACK-BOX TESTS END")
 print ("All tests have passed successfully if there is no error message above.")

cfe63b567f2a124888e000492c47ff448b2d9dad
wojfil <wojfil@protonmail.com>
2022-07-23 23:38:19 +0200

    Introduction of black-box tests

---
+++ src/test/blackbox/run.py
@@ -0,0 +1,24 @@
+import subprocess
+
+def runTestCase(code, expectedResult):
+  cmd = "uro -c \"" + code + "\""
+  output = subprocess.getoutput(cmd)
+  if output != expectedResult:
+    print("Test failed at running code: " + code)
+    print("  Expected output: " + expectedResult)
+    print("  Received output: " + output)
+    
+def lines(*args):
+  return "\n".join(args)
+  
+  
+print ("BLACK-BOX TESTS START")
+	
+runTestCase("print 'hello world'", "hello world")
+runTestCase("print 2 +2*2", "6")
+runTestCase("print 2, 5", lines("2", "5"))
+runTestCase("print 2, 2*25, 1", lines("2", "50", "1"))
+	
+print ("BLACK-BOX TESTS END")
+print ("All tests have passed successfully if there is no error message above.")
+input("Press Enter to continue...")

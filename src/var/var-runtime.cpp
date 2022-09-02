933c0bae499bdb4bc6421240e54690dfd091add6
wojfil <wojfil@protonmail.com>
2022-09-02 21:29:15 +0200

    Index reach error detection

--- src/var/var-runtime.cpp
+++ src/var/var-runtime.cpp
@@ -183,7 +183,7 @@ _boo Variables::getVarValue(const Token& tk, Generator<_tim>*& result)
 _boo Variables::getVarValue(const Token& tk, Generator<_num>*& result)
 {
    if (tk.value.word.h == this->hashes->HASH_VAR_INDEX) {
-      if (this->uroboros->vars.inner.thisState == ThisState::ts_None) {
+      if (this->uroboros->vars.inner.thisState == ThisState::ts_None && !this->vc->anyAggregate()) {
          throw SyntaxException(str(L"variable '", *tk.value.word.os,
             L"' can be accessed only inside a loop"), tk.line);
       }

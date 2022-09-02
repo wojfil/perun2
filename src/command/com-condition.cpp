10825f586c7c12ef8ee107908d9e061e9f1a9ec3
wojfil <wojfil@protonmail.com>
2022-09-02 22:19:13 +0200

    New structs for conditional statements

--- src/command/com-condition.cpp
+++ src/command/com-condition.cpp
@@ -14,6 +14,7 @@
 
 #include "com-condition.h"
 #include "../uroboros.h"
+#include "../util.h"
 
 
 void CS_If::setMain(Command* com)
@@ -59,5 +60,148 @@ void CS_If::run()
             elseCommand->run();
          }
       }
+   }
+}
+
+
+
+If_Base::If_Base(Generator<_boo>* cond, Command* com)
+   : condition(cond), mainCommand(com) { }
+
+If_Base::~If_Base()
+{
+   delete condition;
+   delete mainCommand;
+}
+
+
+If_Raw::If_Raw(Generator<_boo>* cond, Command* com)
+   : If_Base(cond, com) { }
+
+void If_Raw::run()
+{
+   if (this->condition->getValue()) {
+      this->mainCommand->run();
+   }
+}
+
+
+If_Else::If_Else(Generator<_boo>* cond, Command* com, Command* alt)
+   : If_Base(cond, com), altCommand(alt) { }
+
+If_Else::~If_Else()
+{
+   delete altCommand;
+}
+
+void If_Else::run()
+{
+   if (this->condition->getValue()) {
+      this->mainCommand->run();
+   }
+   else {
+      this->altCommand->run();
+   }
+}
+
+
+If_ElseIf::If_ElseIf(Generator<_boo>* cond, Command* com, Generator<_boo>* altCond, Command* alt)
+   : If_Base(cond, com), altCondition(altCond), altCommand(alt) { }
+
+If_ElseIf::~If_ElseIf()
+{
+   delete altCondition;
+   delete altCommand;
+}
+
+void If_ElseIf::run()
+{
+   if (this->condition->getValue()) {
+      this->mainCommand->run();
+   }
+   else if (this->altCondition->getValue()) {
+      this->altCommand->run();
+   }
+}
+
+
+If_ElseIfElse::If_ElseIfElse(Generator<_boo>* cond, Command* com, Generator<_boo>* altCond, Command* alt, Command* els)
+   : If_Base(cond, com), altCondition(altCond), altCommand(alt), elseCommand(els) { }
+
+If_ElseIfElse::~If_ElseIfElse()
+{
+   delete altCondition;
+   delete altCommand;
+   delete elseCommand;
+}
+
+void If_ElseIfElse::run()
+{
+   if (this->condition->getValue()) {
+      this->mainCommand->run();
+   }
+   else if (this->altCondition->getValue()) {
+      this->altCommand->run();
+   }
+   else {
+      this->elseCommand->run();
+   }
+}
+
+
+If_ManyAlternatives::If_ManyAlternatives(Generator<_boo>* cond, Command* com, 
+      const std::vector<Generator<_boo>*>& altConds, const std::vector<Command*>& altComms)
+   : If_Base(cond, com), altConditions(altConds), altCommands(altComms), altCount(altConds.size()) { }
+
+If_ManyAlternatives::~If_ManyAlternatives()
+{
+   deleteVector(altConditions);
+   deleteVector(altCommands);
+}
+
+
+If_Alts::If_Alts(Generator<_boo>* cond, Command* com, const std::vector<Generator<_boo>*>& altConds, 
+   const std::vector<Command*>& altComms)
+   : If_ManyAlternatives(cond, com, altConds, altComms) { }
+
+void If_Alts::run()
+{
+   if (this->condition->getValue()) {
+      this->mainCommand->run();
+   }
+   else {
+      for (_size i = 0; i < this->altCount; i++) {
+         if (this->altConditions[i]->getValue()) {
+            this->altCommands[i]->run();
+            return;
+         }
+      }
+   }
+}
+
+
+If_AltsElse::If_AltsElse(Generator<_boo>* cond, Command* com, const std::vector<Generator<_boo>*>& altConds, 
+   const std::vector<Command*>& altComms, Command* els)
+   : If_ManyAlternatives(cond, com, altConds, altComms), elseCommand(els) { }
+
+If_AltsElse::~If_AltsElse()
+{
+   delete elseCommand;
+}
+
+void If_AltsElse::run()
+{
+   if (this->condition->getValue()) {
+      this->mainCommand->run();
+   }
+   else {
+      for (_size i = 0; i < this->altCount; i++) {
+         if (this->altConditions[i]->getValue()) {
+            this->altCommands[i]->run();
+            return;
+         }
+      }
+
+      this->elseCommand->run();
    }
 }
--- src/command/com-condition.h
+++ src/command/com-condition.h
@@ -68,4 +68,99 @@ private:
 
 
 
+struct If_Base : Command
+{
+public:
+   If_Base(Generator<_boo>* cond, Command* com);
+   ~If_Base();
+
+protected:
+   Generator<_boo>* condition;
+   Command* mainCommand;
+};
+
+
+struct If_Raw : If_Base
+{
+public:
+   If_Raw(Generator<_boo>* cond, Command* com);
+   void run() override;
+};
+
+
+struct If_Else : If_Base
+{
+public:
+   If_Else(Generator<_boo>* cond, Command* com, Command* alt);
+   ~If_Else();
+   void run() override;
+
+private:
+   Command* altCommand;
+};
+
+
+struct If_ElseIf : If_Base
+{
+public:
+   If_ElseIf(Generator<_boo>* cond, Command* com, Generator<_boo>* altCond, Command* alt);
+   ~If_ElseIf();
+   void run() override;
+
+private:
+   Generator<_boo>* altCondition;
+   Command* altCommand;
+};
+
+
+struct If_ElseIfElse : If_Base
+{
+public:
+   If_ElseIfElse(Generator<_boo>* cond, Command* com, Generator<_boo>* altCond, Command* alt, Command* els);
+   ~If_ElseIfElse();
+   void run() override;
+
+private:
+   Generator<_boo>* altCondition;
+   Command* altCommand;
+   Command* elseCommand;
+};
+
+
+struct If_ManyAlternatives : If_Base
+{
+public:
+   If_ManyAlternatives(Generator<_boo>* cond, Command* com, 
+      const std::vector<Generator<_boo>*>& altConds, const std::vector<Command*>& altComms);
+   ~If_ManyAlternatives();
+
+protected:
+   std::vector<Generator<_boo>*> altConditions;
+   std::vector<Command*> altCommands;
+   const _size altCount;
+};
+
+
+struct If_Alts : If_ManyAlternatives
+{
+public:
+   If_Alts(Generator<_boo>* cond, Command* com, const std::vector<Generator<_boo>*>& altConds, 
+      const std::vector<Command*>& altComms);
+   void run() override;
+};
+
+
+struct If_AltsElse : If_ManyAlternatives
+{
+public:
+   If_AltsElse(Generator<_boo>* cond, Command* com, const std::vector<Generator<_boo>*>& altConds, 
+      const std::vector<Command*>& altComms, Command* els);
+   ~If_AltsElse();
+   void run() override;
+
+private:
+   Command* elseCommand;
+};
+
+
 #endif // COM_CONDITION_H_INCLUDED

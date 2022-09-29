07124b235ce077939a1f864b345edaa47f8c2a02
wojfil <wojfil@protonmail.com>
2022-09-29 22:49:08 +0200

    Added missing license mentions

--- src/command/com-misc.h
+++ src/command/com-misc.h
@@ -1,15 +1,15 @@
 /*
-    This file is part of uro::Uroboros.
-    uro::Uroboros is free software: you can redistribute it and/or modify
+    This file is part of Uroboros.
+    Uroboros is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    uro::Uroboros is distributed in the hope that it will be useful,
+    Uroboros is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with uro::Uroboros. If not, see <http://www.gnu.org/licenses/>.
+    along with Uroboros. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef COM_MISC_H

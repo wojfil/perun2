2a82d73f2c0b38ea2060b86588b38d11c12e05c7
wojfil <wojfil@protonmail.com>
2022-12-08 18:58:41 +0100

    Char constants

--- src/datatype/chars.h
+++ src/datatype/chars.h
@@ -24,8 +24,54 @@ namespace uro
 void toLower(_char& ch);
 void toUpper(_char& ch);
 
+inline constexpr _char CHAR_COMMA =                  L',';
+inline constexpr _char CHAR_EXCLAMATION_MARK =       L'!';
+inline constexpr _char CHAR_EQUAL_SIGN =             L'=';
+inline constexpr _char CHAR_OPENING_ROUND_BRACKET =  L'(';
+inline constexpr _char CHAR_CLOSING_ROUND_BRACKET =  L')';
+inline constexpr _char CHAR_OPENING_CURLY_BRACKET =  L'{';
+inline constexpr _char CHAR_CLOSING_CURLY_BRACKET =  L'}';
+inline constexpr _char CHAR_OPENING_SQUARE_BRACKET = L'[';
+inline constexpr _char CHAR_CLOSING_SQUARE_BRACKET = L']';
+inline constexpr _char CHAR_COLON =                  L':';
+inline constexpr _char CHAR_SEMICOLON =              L';';
+inline constexpr _char CHAR_MINUS =                  L'-';
+inline constexpr _char CHAR_PLUS =                   L'+';
+inline constexpr _char CHAR_ASTERISK =               L'*';
+inline constexpr _char CHAR_PERCENT =                L'%';
+inline constexpr _char CHAR_SLASH =                  L'/';
+inline constexpr _char CHAR_BACKSLASH =              L'\\';
+inline constexpr _char CHAR_SMALLER =                L'<';
+inline constexpr _char CHAR_GREATER =                L'>';
+inline constexpr _char CHAR_QUESTION_MARK =          L'?';
+inline constexpr _char CHAR_NEW_LINE =               L'\n';
+inline constexpr _char CHAR_DOT =                    L'.';
+inline constexpr _char CHAR_UNDERSCORE =             L'_';
+inline constexpr _char CHAR_CARET =                  L'^';
+inline constexpr _char CHAR_AMPERSAND =              L'&';
+inline constexpr _char CHAR_VERTICAL_BAR =           L'|';
+inline constexpr _char CHAR_BACKTICK =               L'`';
+inline constexpr _char CHAR_SPACE =                  L' ';
+inline constexpr _char CHAR_QUOTATION_MARK =         L'"';
+inline constexpr _char CHAR_APOSTROPHE =             L'\'';
 
+inline constexpr _char LETTER_b =                    L'b';
+inline constexpr _char LETTER_B =                    L'B';
+inline constexpr _char LETTER_k =                    L'k';
+inline constexpr _char LETTER_K =                    L'K';
+inline constexpr _char LETTER_m =                    L'm';
+inline constexpr _char LETTER_M =                    L'M';
+inline constexpr _char LETTER_g =                    L'g';
+inline constexpr _char LETTER_G =                    L'G';
+inline constexpr _char LETTER_t =                    L't';
+inline constexpr _char LETTER_T =                    L'T';
+inline constexpr _char LETTER_p =                    L'P';
+inline constexpr _char LETTER_P =                    L'P';
 
+inline constexpr _char CHAR_UNARY_MINUS =            L'~';
+// within a numerical expression, the minus sign means either subtraction operation (x-y) or unary negation (-x)
+// this sign is used by the interpreter to distinguish them
+// is used safely internally and wlll not cause collisions if the ~ is added as a regular language operator
 
 
 }

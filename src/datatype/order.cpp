2634f856f60040fb01b688dbf8f30ce36af9fbf0
wojfil <wojfil@protonmail.com>
2023-04-06 01:33:48 +0200

    Move order definitions away from header

--- src/datatype/order.cpp
+++ src/datatype/order.cpp
@@ -34,6 +34,64 @@ void OrderIndices::prepare(const _size length)
    this->values = new _size[length]();
 }
 
+OrderBy::OrderBy(_indptr& inds, _ordptr& ord)
+   : indices(std::move(inds)), order(std::move(ord)) { };
+
+void OrderBy::quicksort(const _int start, const _int end)
+{
+   if (start < end) {
+      const _int i = partition(start, end);
+      quicksort(start, i - 1);
+      quicksort(i + 1, end);
+   }
+}
+
+_int OrderBy::partition(const _int start, const _int end)
+{
+   _int i = start - 1;
+
+   for (_int j = start; j <= end - 1; j++) {
+      if (this->order->matchesSwap(j, end)) {
+         i++;
+         std::swap(this->indices->values[i], this->indices->values[j]);
+         std::iter_swap(this->resultPtr->begin() + i, this->resultPtr->begin() + j);
+      }
+   }
+
+   const _int ip = i + 1;
+   std::swap(this->indices->values[ip], this->indices->values[end]);
+   std::iter_swap(this->resultPtr->begin() + ip, this->resultPtr->begin() + end);
+   return ip;
+}
+
+OrderBy_List::OrderBy_List(_genptr<_list>& bas, _fcptr& ctx, _indptr& inds, _ordptr& ord, _p2& p2)
+      : OrderBy(inds, ord), context(std::move(ctx)), base(std::move(bas)) { };
+
+_list OrderBy_List::getValue()
+{
+   _list result = this->base->getValue();
+   const _size length = result.size();
+
+   if (length == 0) {
+      return result;
+   }
+
+   this->resultPtr = &result;
+   this->indices->prepare(length);
+   this->order->clearValues(length);
+   this->context->resetIndex();
+
+   for (_size i = 0; i < length; i++) {
+      this->context->loadData(result[i]);
+      this->indices->values[i] = i;
+      this->order->addValues();
+      this->context->incrementIndex();
+   }
+
+   this->quicksort(0, length - 1);
+   return result;
+}
+
 OrderBy_Definition::OrderBy_Definition(_defptr& bas, FileContext* ctx, _fcptr& nextCtx, _indptr& inds, _ordptr& ord, _p2& p2)
    : OrderBy(inds, ord), fileContext(ctx), base(std::move(bas)), perun2(p2), nextContext(std::move(nextCtx))
 {

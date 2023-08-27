48c71b7fcc19b8d3d101babc4c8577a8b4f25391
wojfil <wojfil@protonmail.com>
2023-08-27 22:29:35 +0200

    Logic enum

---
+++ src/logic.h
@@ -0,0 +1,34 @@
+/*
+    This file is part of Perun2.
+    Perun2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Peruns2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Perun2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef LOGIC_H_INCLUDED
+#define LOGIC_H_INCLUDED
+
+
+namespace perun2
+{
+
+enum Logic
+{
+   Unknown = -1,
+   False = 0,
+   True = 1
+};
+
+// these numbers above are actually important
+// do not remove them
+
+}
+
+#endif // LOGIC_H_INCLUDED

8c5d732cd6efd462874713382f719f6f44a455e9
wojfil <wojfil@protonmail.com>
2023-09-13 23:41:19 +0200

    First registry iterator

---
+++ src/programs/registry.cpp
@@ -0,0 +1,95 @@
+/*
+    This file is part of Perun2.
+    Perun2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Peruns2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Perun2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "registry.h"
+#include "../logger.h"
+
+
+namespace perun2::prog
+{
+
+
+RegistryIterator::RegistryIterator(const RegistryRootType type, p_genptr<p_str>& rt)
+   : rootType(type), root(std::move(rt)) { };
+
+
+p_str RegistryIterator::getValue()
+{
+   return this->value;
+}
+
+
+HKEY RegistryIterator::getRootKey() const
+{
+   switch (this->rootType) {
+      case RegistryRootType::ClassesRoot: {
+         return HKEY_CLASSES_ROOT;
+      }
+      case RegistryRootType::CurrentUser: {
+         return HKEY_CURRENT_USER;
+      }
+      default: { // case RegistryRootType::LocalMachine
+         return HKEY_LOCAL_MACHINE;
+      }
+   }
+}
+
+
+MultiRegistry::MultiRegistry(const RegistryRootType type, p_genptr<p_str>& rt, const p_str& pattern)
+   : RegistryIterator(type, rt) { };
+
+
+void MultiRegistry::reset()
+{
+   if (! this->first) {
+      this->first = true;
+      RegCloseKey(hKey);
+   }
+}
+
+
+p_bool MultiRegistry::hasNext()
+{
+   if (this->first) {
+      this->result = RegOpenKeyExW(this->getRootKey(), this->root->getValue().c_str(), 0, KEY_READ, &hKey);
+
+      if (this->result != ERROR_SUCCESS) {
+         return false;
+      }
+
+      this->first = false;
+   }
+
+   result = RegEnumKeyExW(hKey, index, subkeyName, &subkeyNameSize, NULL, NULL, NULL, NULL);
+
+   if (result != ERROR_SUCCESS) {
+      this->reset();
+      return false;
+   }
+
+   this->value = subkeyName;
+   subkeyNameSize = MAX_PATH;
+   index++;
+
+   this->reset();
+   return true;
+}
+
+
+
+
+
+
+
+}
---
+++ src/programs/registry.h
@@ -0,0 +1,78 @@
+/*
+    This file is part of Perun2.
+    Perun2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Peruns2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Perun2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef REGISTRY_H_INCLUDED
+#define REGISTRY_H_INCLUDED
+
+#include "../datatype/generator.h"
+#include <windows.h>
+
+
+namespace perun2::prog
+{
+
+
+enum RegistryRootType
+{
+   ClassesRoot,
+   CurrentUser,
+   LocalMachine
+};
+
+
+struct RegistryIterator : Generator<p_str>
+{
+public:
+   RegistryIterator(const RegistryRootType type, p_genptr<p_str>& rt);
+   virtual void reset() = 0;
+   virtual p_bool hasNext() = 0;
+
+   p_str getValue() override;
+
+protected:
+   HKEY getRootKey() const;
+
+   p_genptr<p_str> root;
+   p_str value;
+
+private:
+   const RegistryRootType rootType;
+};
+
+
+typedef std::unique_ptr<RegistryIterator> p_riptr;
+
+
+struct MultiRegistry : RegistryIterator
+{
+public:
+   MultiRegistry(const RegistryRootType type, p_genptr<p_str>& rt, const p_str& pattern);
+   void reset() override;
+   p_bool hasNext() override;
+
+private:
+   p_bool first = true;
+
+   HKEY hKey;
+   LONG result;
+   DWORD index = 0;
+   WCHAR subkeyName[MAX_PATH];
+   DWORD subkeyNameSize = MAX_PATH;
+};
+
+
+
+}
+
+#endif // REGISTRY_H_INCLUDED

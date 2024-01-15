5eb16b23c5d30346d2fd95e37ef19a9f3d2c010b
wojfil <wojfil@protonmail.com>
2024-01-15 20:13:50 +0100

    Os windows

---
+++ src/os/os-windows.h
@@ -0,0 +1,29 @@
+/*
+    This file is part of Perun2.
+    Perun2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Perun2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Perun2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OS_WINDOWS_H
+#define OS_WINDOWS_H
+
+#include "../datatype/datatype.h"
+
+
+namespace perun2
+{
+
+
+
+}
+
+
+#endif /* OS_WINDOWS_H */

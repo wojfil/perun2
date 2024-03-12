e4ca01a2e576f5ec70af7947778cb545a0a953b3
wojfil <wojfil@protonmail.com>
2024-03-12 18:38:16 +0100

    Empty files for the Between operator

---
+++ src/datatype/between.h
@@ -0,0 +1,28 @@
+/*
+    This file is part of Perun2.
+    Perun2 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    Perun2 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with Perun2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef BETWEEN_H
+#define BETWEEN_H
+
+
+#include "datatype.h"
+
+
+namespace perun2::gen
+{
+
+
+}
+
+#endif /* BETWEEN_H */
